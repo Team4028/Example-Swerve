58dedd34c7118101299b88d13ac03095420d44ef
CadeReinberger <32687832+CadeReinberger@users.noreply.github.com>
2020-02-08 09:19:57 -0500

    Work towards auton sturcture

--- src/main/java/com/swervedrivespecialties/exampleswerve/auton/Trajectories.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/auton/Trajectories.java
@@ -9,6 +9,8 @@ package com.swervedrivespecialties.exampleswerve.auton;
 
 import java.util.function.Supplier;
 
+import com.swervedrivespecialties.exampleswerve.util.util;
+
 import org.frcteam2910.common.control.CentripetalAccelerationConstraint;
 import org.frcteam2910.common.control.ITrajectoryConstraint;
 import org.frcteam2910.common.control.MaxAccelerationConstraint;
@@ -16,6 +18,7 @@ import org.frcteam2910.common.control.MaxVelocityConstraint;
 import org.frcteam2910.common.control.Path;
 import org.frcteam2910.common.control.PathArcSegment;
 import org.frcteam2910.common.control.PathLineSegment;
+import org.frcteam2910.common.control.PathSegment;
 import org.frcteam2910.common.control.Trajectory;
 import org.frcteam2910.common.math.Rotation2;
 import org.frcteam2910.common.math.Vector2;
@@ -28,79 +31,33 @@ public class Trajectories {
     private static final double kMaxAccel = 13. * 12;
     private static final double kMaxCentripedalAccel = 25. * 12;
 
-    ////////////// TEST TRAJECTORY ////////////////////////
-    private static Trajectory testTrajectory;
-    private static final double testTrajectoryMaxVelo = 6 * 12;
-    private static final ITrajectoryConstraint[] testTrajectoryConstraints = {new MaxVelocityConstraint(testTrajectoryMaxVelo), 
-                                                                             new MaxAccelerationConstraint(kMaxAccel), 
-                                                                             new CentripetalAccelerationConstraint(kMaxCentripedalAccel)};
-    private static final double testTrajectoryEndVelo = 3 * 12;
-    private static final Rotation2 testPathRotation = Rotation2.fromDegrees(-90.);
-    private static final Rotation2 testPathStartRotation = Rotation2.ZERO;
 
-    private static void generateTestTrajectory(){
-        Path testPath = new Path(testPathStartRotation);
-        testPath.addSegment(new PathLineSegment(Vector2.ZERO, new Vector2(24, 0)), testPathRotation);
-        testPath.addSegment(new PathArcSegment(new Vector2(24, 0), new Vector2(72, -48), new Vector2(24, -48)), testPathRotation);
-        testPath.subdivide(kSubdivideIterations);
-        testTrajectory = new Trajectory(0.0, testTrajectoryEndVelo, testPath, testTrajectoryConstraints);
+    public static class steallBallAuton {
+        private static final Vector2 startingPos = new Vector2(11, 23); //The origin is a theoretical point in the back right corner of the field. 
+        private static final Vector2 shootPos = new Vector2(100, 200);
+        private static final double stealBallX = 120;
+        private static final double ly = 47.5;
+        private static final double stealBallSpeed = 10.0;
+        private static final double goToShootSpeed = 10.0;
+
+        private static final double lx = stealBallX - startingPos.x;
+        private static final double lambda = (lx * lx + ly * ly)  / (2.0 * ly);
+
+        private static final ITrajectoryConstraint[] toStealBallConstraints = getConstraint(stealBallSpeed);
+        
+        
+
+
+
+
     }
-
-    private static Trajectory getTestTrajectory(){
-        return testTrajectory;
-    }
-
-    public static Supplier<Trajectory> testTrajectorySupplier = () -> getTestTrajectory();
-    ////////////////////////////////////////////////////////
-
-
-    //////////////// EXAMPLE TRAJECTORY //////////////////
-    private static Trajectory exampleTrajectory;
-    private static final double exampleTrajectoryX = 42;
-    private static final double exampleTrajectoryY = -24;
-    private static final double exampleTrajectoryPhi = -60; //degrees
-
-    private static void generateExampleTrajectory(){
-        exampleTrajectory = generateLineTrajectory(new Vector2(exampleTrajectoryX, exampleTrajectoryY), Rotation2.fromDegrees(exampleTrajectoryPhi));
-    }
-    
-    private static Trajectory getExampleTrajectory(){
-        return exampleTrajectory;
-    }
-
-    public static Supplier<Trajectory> exampleTrajectorySupplier = () -> getExampleTrajectory();
-    //////////////////////////////////////////////////////
 
 
     public static void generateAllTrajectories(){
-        generateTestTrajectory();
-        generateExampleTrajectory();
     }
 
 
     /////////////////// Helper Methods /////////////////////////
-
-    private static Trajectory generateLineTrajectory(Vector2 line, Rotation2 endRotation){
-        ITrajectoryConstraint[] lineTrajectoryConstraints = {new MaxVelocityConstraint(kDefaultMaxSpeed), 
-                                                             new MaxAccelerationConstraint(kMaxAccel), 
-                                                             new CentripetalAccelerationConstraint(kMaxCentripedalAccel)};
-        Path linePath = new Path(Rotation2.ZERO);
-        linePath.addSegment(new PathLineSegment(Vector2.ZERO, line), endRotation);
-        linePath.subdivide(kSubdivideIterations);
-        Trajectory resultTrajectory = new Trajectory(0.0, 0.0, linePath, lineTrajectoryConstraints);
-        return resultTrajectory;
-    }
-
-    public static Trajectory generateLineTrajectory(Vector2 line, Rotation2 startRotation, Rotation2 endRotation){
-        ITrajectoryConstraint[] lineTrajectoryConstraints = {new MaxVelocityConstraint(kDefaultMaxSpeed), 
-                                                             new MaxAccelerationConstraint(kMaxAccel), 
-                                                             new CentripetalAccelerationConstraint(kMaxCentripedalAccel)};
-        Path linePath = new Path(startRotation);
-        linePath.addSegment(new PathLineSegment(Vector2.ZERO, line), endRotation);
-        linePath.subdivide(kSubdivideIterations);
-        Trajectory resultTrajectory = new Trajectory(0.0, 0.0, linePath, lineTrajectoryConstraints);
-        return resultTrajectory;
-    }
 
     public static Trajectory generateLineTrajectory(Vector2 line, double speed,  Rotation2 startRotation, Rotation2 endRotation){
         ITrajectoryConstraint[] lineTrajectoryConstraints = {new MaxVelocityConstraint(speed), 
@@ -111,5 +68,18 @@ public class Trajectories {
         linePath.subdivide(kSubdivideIterations);
         Trajectory resultTrajectory = new Trajectory(0.0, 0.0, linePath, lineTrajectoryConstraints);
         return resultTrajectory;
+    }
+
+    public static Trajectory generateLineTrajectory(Vector2 line, Rotation2 startRotation, Rotation2 endRotation){
+        return generateLineTrajectory(line, kDefaultMaxSpeed, startRotation, endRotation);
+    }
+
+    private static Trajectory generateLineTrajectory(Vector2 line, Rotation2 endRotation){
+        return generateLineTrajectory(line, Rotation2.ZERO, endRotation);
+    }
+
+    private static ITrajectoryConstraint[] getConstraint(double spd){
+        ITrajectoryConstraint[] res = {new MaxVelocityConstraint(spd), new MaxAccelerationConstraint(kMaxAccel), new CentripetalAccelerationConstraint(kMaxCentripedalAccel)};
+        return res;
     }
 }
