44f274c15e19b4c6876d1b3f2c9b4b234e9ca729
CadeReinberger <32687832+CadeReinberger@users.noreply.github.com>
2020-02-11 20:02:10 -0500

    yeet

--- src/main/java/com/swervedrivespecialties/exampleswerve/Robot.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/Robot.java
@@ -1,5 +1,7 @@
 package com.swervedrivespecialties.exampleswerve;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.swervedrivespecialties.exampleswerve.auton.Trajectories;
 import com.swervedrivespecialties.exampleswerve.subsystems.DrivetrainSubsystem;
 import com.swervedrivespecialties.exampleswerve.subsystems.Infeed;
@@ -37,7 +39,7 @@ public class Robot extends TimedRobot{
 
     @Override
     public void autonomousPeriodic() {
-        DrivetrainSubsystem.getInstance().drive(new Translation2d(0., 0), 0, true);
+        //DrivetrainSubsystem.getInstance().drive(new Translation2d(0., 0), 0, true);
     }
 
     @Override
--- src/main/java/com/swervedrivespecialties/exampleswerve/subsystems/Infeed.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/subsystems/Infeed.java
@@ -26,7 +26,7 @@ public class Infeed extends SubsystemBase {
   public static final double kEncoderCountsPerBall = 7500;
   private static final double kConveyorTalonConstantVBus = -0.35 ;
   private static final double kConveyToShootConstantVBUS = -.7;
-  private static final double kInfeedVBus = 1;
+  private static final double kInfeedVBus = .5;
   private static final double kSingulatorVBus = -.45;
   private static final double kSingulateToShootVBus = -.5;
 
